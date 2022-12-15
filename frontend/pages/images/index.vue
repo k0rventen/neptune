2408c2a291e79e24f236e416208bd56c6ce7f82b
Kalv1 <calvin.lambert@protonmail.com>
2022-12-15 16:42:46 +0100

    :art: Reformat file & remove loading on image page

--- frontend/pages/images/index.vue
+++ frontend/pages/images/index.vue
@@ -1,18 +1,41 @@
 <template>
-  <div class="w-full px-5 py-5 text-white h-screen scrollbar-thin overflow-auto">
-    <Loading v-if="isLoading === true"/>
+  <div
+    class="w-full px-5 py-5 text-white h-screen scrollbar-thin overflow-auto"
+  >
     <div class="text-gray-400">
-      <input v-model="search" type="text" class="w-full mb-5 px-6 py-3 rounded-full shadow-md outline-none" placeholder="Rechercher ...">
+      <input
+        v-model="search"
+        type="text"
+        class="w-full mb-5 px-6 py-3 rounded-full shadow-md outline-none"
+        placeholder="Rechercher ..."
+      />
     </div>
     <div class="w-full grid grid-cols-3 gap-9">
-        <div v-for="(tag,index) in value" :key="index" class="col-span-3 md:col-span-1 w-full cursor-pointer shadow-md bg-neptune-blue px-5 rounded-lg py-5 relative overflow-hidden" :class="selectColor(tag.vulnerabilities, tag.outdated_packages)" @click="$router.push({ path: '/images/' + tag.sha } )">
-          <svg class="absolute opacity-20 right-0 -bottom-8" width="128" height="128" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" fill="white"><path d="M11.5 23l-8.5-4.535v-3.953l5.4 3.122 3.1-3.406v8.772zm1-.001v-8.806l3.162 3.343 5.338-2.958v3.887l-8.5 4.534zm-10.339-10.125l-2.161-1.244 3-3.302-3-2.823 8.718-4.505 3.215 2.385 3.325-2.385 8.742 4.561-2.995 2.771 2.995 3.443-2.242 1.241v-.001l-5.903 3.27-3.348-3.541 7.416-3.962-7.922-4.372-7.923 4.372 7.422 3.937v.024l-3.297 3.622-5.203-3.008-.16-.092-.679-.393v.002z"/></svg>
-          <p>Nom de l'image : {{tag.image + ':' + tag.tag}}</p>
-          <p>Taille de l'image : {{ calcSize(tag.size) }}</p>
-          <p>Paquets : {{ tag.packages }}</p>
-          <p>Vulnérabilité : {{ tag.vulnerabilities }}</p>
-          <p>Paquet obsolète : {{ tag.outdated_packages }}</p>
-        </div>
+      <div
+        v-for="(tag, index) in value"
+        :key="index"
+        class="col-span-3 md:col-span-1 w-full cursor-pointer shadow-md bg-neptune-blue px-5 rounded-lg py-5 relative overflow-hidden"
+        :class="selectColor(tag.vulnerabilities, tag.outdated_packages)"
+        @click="$router.push({ path: '/images/' + tag.sha })"
+      >
+        <svg
+          class="absolute opacity-20 right-0 -bottom-8"
+          width="128"
+          height="128"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="white"
+        >
+          <path
+            d="M11.5 23l-8.5-4.535v-3.953l5.4 3.122 3.1-3.406v8.772zm1-.001v-8.806l3.162 3.343 5.338-2.958v3.887l-8.5 4.534zm-10.339-10.125l-2.161-1.244 3-3.302-3-2.823 8.718-4.505 3.215 2.385 3.325-2.385 8.742 4.561-2.995 2.771 2.995 3.443-2.242 1.241v-.001l-5.903 3.27-3.348-3.541 7.416-3.962-7.922-4.372-7.923 4.372 7.422 3.937v.024l-3.297 3.622-5.203-3.008-.16-.092-.679-.393v.002z"
+          />
+        </svg>
+        <p>Nom de l'image : {{ tag.image + ':' + tag.tag }}</p>
+        <p>Taille de l'image : {{ calcSize(tag.size) }}</p>
+        <p>Paquets : {{ tag.packages }}</p>
+        <p>Vulnérabilité : {{ tag.vulnerabilities }}</p>
+        <p>Paquet obsolète : {{ tag.outdated_packages }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +45,7 @@ import { mapActions, mapState } from 'vuex'
 import calcSize from '@/mixins/sizeCalc.js'
 
 export default {
-  name: "ImagesPage",
+  name: 'ImagesPage',
   mixins: [calcSize],
   data() {
     return {
@@ -30,7 +53,7 @@ export default {
       imageToScan: undefined,
       search: undefined,
       openModalAdd: false,
-      isLoading: true
+      isLoading: true,
     }
   },
   computed: {
@@ -38,7 +61,7 @@ export default {
   },
   watch: {
     search(newValue) {
-      if(newValue === '') {
+      if (newValue === '') {
         this.value = this.tags
       } else {
         this.value = this.tags.filter((el) => {
@@ -49,11 +72,13 @@ export default {
     },
   },
   async mounted() {
-    await this.getTags().then(() => {
-      this.value = this.tags
-    }).finally(() => {
-      this.isLoading = false
-    })
+    await this.getTags()
+      .then(() => {
+        this.value = this.tags
+      })
+      .finally(() => {
+        this.isLoading = false
+      })
   },
   methods: {
     ...mapActions('image', ['getImages', 'getTags']),
@@ -62,16 +87,15 @@ export default {
         this.imageToScan = undefined
       })
     },
-    selectColor(vuln, outdated){
-      if(vuln > 0) {
+    selectColor(vuln, outdated) {
+      if (vuln > 0) {
         return 'bg-red-400'
       }
-      if(outdated > 0) {
+      if (outdated > 0) {
         return 'bg-yellow-400'
       }
       return 'bg-neptune-blue'
     },
-  }
+  },
 }
 </script>
-
--- frontend/pages/vulnerabilities/index.vue
+++ frontend/pages/vulnerabilities/index.vue
@@ -1,80 +1,124 @@
 <template>
   <div class="w-full h-screen overflow-y-auto scrollbar-thin px-5 py-5">
-
     <label class="relative">
-      <input v-model="search" type="text" class="w-full mb-5 px-6 py-3 rounded-full shadow-md outline-none" placeholder="Rechercher ...">
+      <input
+        v-model="search"
+        type="text"
+        class="w-full mb-5 px-6 py-3 rounded-full shadow-md outline-none"
+        placeholder="Rechercher ..."
+      />
     </label>
 
     <div class="w-full shadow-md rounded-xl bg-white px-3 py-3">
       <Table
-      v-if="cveTable.length > 0"
-      :columns="[
-        {label: $t('vulnerability.name'), name: 'name', sorter: (row1 , row2) => row1.name.localeCompare(row2.name) },
-        {label: $t('vulnerability.severity'), name: 'severity', sorter: (row1, row2) => {
-          const severity = {
-            'critical': 1,
-            'high': 2,
-            'medium': 3,
-            'low': 4,
-            'negligible': 5,
-            'unknown': 6
-          }
-          return severity[row1.severity.toLowerCase()] - severity[row2.severity.toLowerCase()]
-        }},
-        {label: $t('vulnerability.source'), name: 'source'},
-        {label: $t('vulnerability.affected_tags'), name: 'affected_images', sorter: (row1, row2) => row1.affected_images.length - row2.affected_images.length },
-        {label: $t('vulnerability.notes'), name: 'notes'},
-        {label: $t('vulnerability.acknowledged'), name: 'ack', sorter: (row1, row2) => (row1.ack === row2.ack) ? 0 : x ? -1 : 1},
-      ]"
-      :data="cveTable"
-    >
-      <template slot="severity" slot-scope="{item}">
-        <div class="w-full flex justify-center">
-          <div class="px-5 py-2 rounded-full text-white w-36 uppercase font-bold" :class="colorSeverity(item)">
-            <p>{{ $t(`vulnerability.state.${item.severity.toLowerCase()}`) }}</p>
+        v-if="cveTable.length > 0"
+        :columns="[
+          {
+            label: $t('vulnerability.name'),
+            name: 'name',
+            sorter: (row1, row2) => row1.name.localeCompare(row2.name),
+          },
+          {
+            label: $t('vulnerability.severity'),
+            name: 'severity',
+            sorter: (row1, row2) => {
+              const severity = {
+                critical: 1,
+                high: 2,
+                medium: 3,
+                low: 4,
+                negligible: 5,
+                unknown: 6,
+              }
+              return (
+                severity[row1.severity.toLowerCase()] -
+                severity[row2.severity.toLowerCase()]
+              )
+            },
+          },
+          { label: $t('vulnerability.source'), name: 'source' },
+          {
+            label: $t('vulnerability.affected_tags'),
+            name: 'affected_images',
+            sorter: (row1, row2) =>
+              row1.affected_images.length - row2.affected_images.length,
+          },
+          { label: $t('vulnerability.notes'), name: 'notes' },
+          {
+            label: $t('vulnerability.acknowledged'),
+            name: 'ack',
+            sorter: (row1, row2) => (row1.ack === row2.ack ? 0 : x ? -1 : 1),
+          },
+        ]"
+        :data="cveTable"
+      >
+        <template slot="severity" slot-scope="{ item }">
+          <div class="w-full flex justify-center">
+            <div
+              class="px-5 py-2 rounded-full text-white w-36 uppercase font-bold"
+              :class="colorSeverity(item)"
+            >
+              <p>
+                {{ $t(`vulnerability.state.${item.severity.toLowerCase()}`) }}
+              </p>
+            </div>
           </div>
-        </div>
-      </template>
-      <template slot="name" slot-scope="{item}">
-        <a target="_blank" :href="`https://www.google.com/search?q=${item.name}`">
-          <p>{{ item.name }}</p>
-        </a>
-      </template>
-      <template slot="notes" slot-scope="{item}">
-        <input v-model="copyNote[item.name]" class="w-full outline-none bg-transparent text-center" type="text" @focusout="sendNewNotes(item)">
-      </template>
-      <template slot="affected_images" slot-scope="{item}">
-        <VDropdown
-          :distance="6"
-        >
-          <button class="px-5 py-2 bg-[#A6D1E6] rounded-full text-white uppercase font-bold">
-            {{ $t('vulnerability.tags') }}
+        </template>
+        <template slot="name" slot-scope="{ item }">
+          <a
+            target="_blank"
+            :href="`https://www.google.com/search?q=${item.name}`"
+          >
+            <p>{{ item.name }}</p>
+          </a>
+        </template>
+        <template slot="notes" slot-scope="{ item }">
+          <input
+            v-model="copyNote[item.name]"
+            class="w-full outline-none bg-transparent text-center"
+            type="text"
+            @focusout="sendNewNotes(item)"
+          />
+        </template>
+        <template slot="affected_images" slot-scope="{ item }">
+          <VDropdown :distance="6">
+            <button
+              class="px-5 py-2 bg-[#A6D1E6] rounded-full text-white uppercase font-bold"
+            >
+              {{ $t('vulnerability.tags') }}
+            </button>
+
+            <template #popper>
+              <p>{{ item }}</p>
+            </template>
+          </VDropdown>
+        </template>
+        <template slot="ack" slot-scope="{ item }">
+          <button
+            class="px-5 py-2 bg-[#A6D1E6] rounded-full text-white uppercase font-bold"
+            :class="item.active ? 'bg-green-600' : 'bg-gray-400'"
+            @click="changeAckState(item)"
+          >
+            {{
+              item.active
+                ? $t('vulnerability.acknowledged')
+                : $t('vulnerability.not_acknowledged')
+            }}
           </button>
-
-          <template #popper>
-            <p>{{ item }}</p>
-          </template>
-        </VDropdown>
-
-      </template>
-      <template slot="ack" slot-scope="{item}">
-        <button class="px-5 py-2 bg-[#A6D1E6] rounded-full text-white uppercase font-bold" :class="item.active ? 'bg-green-600' : 'bg-gray-400'" @click="changeAckState(item)">
-          {{ item.active ? $t('vulnerability.acknowledged') : $t('vulnerability.not_acknowledged') }}
-        </button>
-      </template>
-    </Table>
+        </template>
+      </Table>
     </div>
     <div class="flex justify-center w-full mt-3">
-      <Pagination v-model="pages" :nbPages="5"/>
+      <Pagination v-model="pages" :nb-pages="5" />
     </div>
   </div>
 </template>
 
 <script>
-import { mapState, mapActions } from "vuex";
+import { mapState, mapActions } from 'vuex'
 
 export default {
-  name: "VunerabilitiesPage",
+  name: 'VunerabilitiesPage',
   data() {
     return {
       copyNote: [],
@@ -84,18 +128,26 @@ export default {
     }
   },
   computed: {
-    ...mapState('vulnerability', ['vuln', 'notes'])
+    ...mapState('vulnerability', ['vuln', 'notes']),
   },
   async mounted() {
     await this.getVulnerabilties().then(() => {
       this.copyNote = this.notes
       this.cveTable = this.vuln
-    });
+    })
   },
   methods: {
-    ...mapActions('vulnerability', ['getVulnerabilties', 'setNotes', 'setAckState']),
+    ...mapActions('vulnerability', [
+      'getVulnerabilties',
+      'setNotes',
+      'setAckState',
+    ]),
     sendNewNotes(item) {
-      this.setNotes({ cve: item.id, notes: this.copyNote[item.name], active: item.active })
+      this.setNotes({
+        cve: item.id,
+        notes: this.copyNote[item.name],
+        active: item.active,
+      })
     },
     colorSeverity(item) {
       switch (item.severity) {
@@ -114,12 +166,14 @@ export default {
       }
     },
     changeAckState(item) {
-      this.setAckState({ cve: item.id, notes: this.copyNote[item.name], active: !item.active })
+      this.setAckState({
+        cve: item.id,
+        notes: this.copyNote[item.name],
+        active: !item.active,
+      })
     },
   },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
