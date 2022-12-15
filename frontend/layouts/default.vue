da724a4fe372b619988dfe43c5b0ccfbdb622cdb
Kalv1 <calvin.lambert@protonmail.com>
2022-12-15 14:41:54 +0100

    :iphone: Work on layout of app in mobile & tablet

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -1,5 +1,13 @@
 <template>
-  <div class="bg-primary w-full h-screen overflow-hidden flex">
+  <div class="bg-primary w-full h-screen overflow-hidden lg:flex">
+    <Navbar class="lg:hidden" 
+    :elements="[
+      { name: $t('navbar.home'), route: '/' },
+      { name: $t('navbar.images'), route: '/images' },
+      { name: $t('navbar.vulnerabilities'), route: '/vulnerabilities' },
+      { name: $t('navbar.dependencies'), route: '/dependencies' }
+    ]"
+    />
     <div class="w-1/5 bg-red-600 h-screen hidden lg:block">
       <Sidebar :elements="[
       { name: $t('navbar.home'), route: '/' },
--- frontend/pages/dependencies/index.vue
+++ frontend/pages/dependencies/index.vue
@@ -1,24 +1,39 @@
 <template>
-  <div class="w-full">
-    <Table
-      v-if="dependencies.length > 0"
-      :columns="[
-        {label: $t('dependencies.type'), name: 'type'},
-        {label: $t('dependencies.name'), name: 'name'},
-        {label: $t('dependencies.vulnerability'), name: 'vuln'},
-        {label: $t('dependencies.version'), name: 'version'},
-        {label: $t('dependencies.minimum_version'), name: 'minimum_version'},
-        {label: $t('dependencies.notes'), name: 'notes'},
-      ]"
-      :data="dependencies"
-    >
-      <template slot="notes" slot-scope="{item}">
-        <input v-model="copyNotes[item.name]" class="w-full outline-none bg-transparent text-center" type="text" @focusout="sendNewNotes(item)">
-      </template>
-      <template slot="minimum_version" slot-scope="{item}">
-        <input v-model="copyMinVersion[item.name]" class="w-full outline-none bg-transparent text-center" type="text" @focusout="sendNewVersion(item)">
-      </template>
-    </Table>
+  <div class="w-full h-screen px-5 py-5 overflow-y-auto scrollbar-thin">
+    <div class="w-full shadow-md rounded-xl bg-white px-3 py-3">
+      <Table
+        v-if="dependencies.length > 0"
+        :columns="[
+          { label: $t('dependencies.type'), name: 'type' },
+          { label: $t('dependencies.name'), name: 'name' },
+          { label: $t('dependencies.vulnerability'), name: 'vuln' },
+          { label: $t('dependencies.version'), name: 'version' },
+          {
+            label: $t('dependencies.minimum_version'),
+            name: 'minimum_version',
+          },
+          { label: $t('dependencies.notes'), name: 'notes' },
+        ]"
+        :data="dependencies"
+      >
+        <template slot="notes" slot-scope="{ item }">
+          <input
+            v-model="copyNotes[item.name]"
+            class="w-full outline-none bg-transparent text-center"
+            type="text"
+            @focusout="sendNewNotes(item)"
+          />
+        </template>
+        <template slot="minimum_version" slot-scope="{ item }">
+          <input
+            v-model="copyMinVersion[item.name]"
+            class="w-full outline-none bg-transparent text-center"
+            type="text"
+            @focusout="sendNewVersion(item)"
+          />
+        </template>
+      </Table>
+    </div>
   </div>
 </template>
 
@@ -26,7 +41,7 @@
 import { mapActions, mapState } from 'vuex'
 
 export default {
-  name: "DependenciesPage",
+  name: 'DependenciesPage',
   data() {
     return {
       version: '',
@@ -44,17 +59,27 @@ export default {
     })
   },
   methods: {
-    ...mapActions('dependencies', ['getDependencies', 'setVersion', 'setNotes']),
+    ...mapActions('dependencies', [
+      'getDependencies',
+      'setVersion',
+      'setNotes',
+    ]),
     sendNewVersion(item) {
-      this.setVersion({ id: item.id, version: this.copyMinVersion[item.name], notes: item.notes })
+      this.setVersion({
+        id: item.id,
+        version: this.copyMinVersion[item.name],
+        notes: item.notes,
+      })
     },
     sendNewNotes(item) {
-      this.setNotes({ id: item.id, version: item.minimum_version, notes: this.copyNotes[item.name] })
+      this.setNotes({
+        id: item.id,
+        version: item.minimum_version,
+        notes: this.copyNotes[item.name],
+      })
     },
   },
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
