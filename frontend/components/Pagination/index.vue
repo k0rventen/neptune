2eabda0e9f106abae8a40358ad047a5d8035d707
Kalv1 <calvin.lambert@protonmail.com>
2022-12-23 13:27:12 +0100

    :bug: Fix pagination bug when index < 5

--- frontend/components/Pagination/index.vue
+++ frontend/components/Pagination/index.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="flex items-center">
+    <div class="flex items-center text-black">
         <svg @click="downgradeOffset()" width="25" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z"/></svg>
         <button v-if="offset > 0" :key="index" class="px-3 py-1">
             ...
         </button>
-        <button v-for="index in 5" :key="index" class="px-3 py-1" :class="{'bg-[#a6d1e6] rounded-md text-white' : index === currentPage - offset }" @click="setPage(index)">
+        <button v-for="index in nbDisplayedPages()" :key="index" class="px-3 py-1" :class="{'bg-[#a6d1e6] rounded-md text-white' : index === currentPage - offset }" @click="setPage(index)">
             {{ index + offset }}
         </button>
-        <button v-if="offset + 5 !== nbPages" :key="index" class="px-3 py-1">
+        <button v-if="offset + 5 !== nbPages && nbPages >= 5" :key="index" class="px-3 py-1">
             ...
         </button>
         <svg @click="upgradeOffset()" width="25" class="rotate-180" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path d="m13.789 7.155c.141-.108.3-.157.456-.157.389 0 .755.306.755.749v8.501c0 .445-.367.75-.755.75-.157 0-.316-.05-.457-.159-1.554-1.203-4.199-3.252-5.498-4.258-.184-.142-.29-.36-.29-.592 0-.23.107-.449.291-.591 1.299-1.002 3.945-3.044 5.498-4.243z"/></svg>
@@ -56,11 +56,20 @@ export default {
             }
         },
         setPage(index) {
-            this.$emit('change', index + this.offset)
+          this.$emit('change', index + this.offset)
+          if(this.nbPages > 5) {
             if(this.offset + index - 1 > this.nbPages - 5) {
                 this.offset = this.nbPages - 5
             } else {
                 this.offset = index + this.offset - 1
+            }
+          } 
+        },
+        nbDisplayedPages() {
+            if(this.nbPages > 5) {
+                return 5
+            } else {
+                return this.nbPages
             }
         }
     }
