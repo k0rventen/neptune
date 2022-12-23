a7f368a9949df2f0d1e868a874bd74ae73681e04
Kalv1 <calvin.lambert@protonmail.com>
2022-12-23 09:17:30 +0100

    :children_crossing: moving button to associate page instead of home page

--- frontend/pages/images/index.vue
+++ frontend/pages/images/index.vue
@@ -2,24 +2,68 @@
   <div
     class="w-full px-5 py-5 pb-28 lg:pb-5 text-white h-screen scrollbar-thin overflow-auto"
   >
+    <Modal
+      v-if="openImagesModal"
+      v-model="openImagesModal"
+      title="Ajout d'une image en direct"
+      class="text-gray-500"
+    >
+      <label for="" class="my-2 gap-3 block">
+        Nom de l'image :
+        <input
+          v-model="imageName"
+          type="text"
+          class="rounded-md border border-gray-400 outline-none px-2 py-1"
+        />
+      </label>
+      <template #footer>
+        <button
+          class="px-3 py-1 rounded-md bg-neptune-blue text-white"
+          @click="sendNewImage()"
+        >
+          Envoyer
+        </button>
+      </template>
+    </Modal>
     <div class="text-gray-400">
-      <div class="relative  mb-2">
+      <div class="relative mb-2">
         <input
           v-model="search"
           type="text"
           class="w-full px-6 py-3 rounded-full shadow-md outline-none"
           placeholder="Rechercher ..."
         />
-        <svg class="absolute right-4 top-1/4 opacity-20" width="26" height="26" clip-rule="evenodd" fill-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="2" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" @click="openFilter = !openFilter"><path d="m15.344 17.778c0-.414-.336-.75-.75-.75h-5.16c-.414 0-.75.336-.75.75s.336.75.75.75h5.16c.414 0 .75-.336.75-.75zm2.206-4c0-.414-.336-.75-.75-.75h-9.596c-.414 0-.75.336-.75.75s.336.75.75.75h9.596c.414 0 .75-.336.75-.75zm2.45-4c0-.414-.336-.75-.75-.75h-14.5c-.414 0-.75.336-.75.75s.336.75.75.75h14.5c.414 0 .75-.336.75-.75zm2-4c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z" fill-rule="nonzero"/></svg>
+        <svg
+          class="absolute right-4 top-1/4 opacity-20"
+          width="26"
+          height="26"
+          clip-rule="evenodd"
+          fill-rule="evenodd"
+          stroke-linejoin="round"
+          stroke-miterlimit="2"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+          @click="openFilter = !openFilter"
+        >
+          <path
+            d="m15.344 17.778c0-.414-.336-.75-.75-.75h-5.16c-.414 0-.75.336-.75.75s.336.75.75.75h5.16c.414 0 .75-.336.75-.75zm2.206-4c0-.414-.336-.75-.75-.75h-9.596c-.414 0-.75.336-.75.75s.336.75.75.75h9.596c.414 0 .75-.336.75-.75zm2.45-4c0-.414-.336-.75-.75-.75h-14.5c-.414 0-.75.336-.75.75s.336.75.75.75h14.5c.414 0 .75-.336.75-.75zm2-4c0-.414-.336-.75-.75-.75h-18.5c-.414 0-.75.336-.75.75s.336.75.75.75h18.5c.414 0 .75-.336.75-.75z"
+            fill-rule="nonzero"
+          />
+        </svg>
       </div>
-      <button class="bg-neptune-blue text-white px-3 py-1 rounded-md mb-2">Ajouter une image</button>
-      
+      <button class="bg-neptune-blue text-white px-3 py-1 rounded-md mb-2" @click="openImagesModal = true">
+        Ajouter une image
+      </button>
+
       <div v-if="openFilter" class="w-full flex gap-3">
         <div class="w-1/6">
-          <label for="trie" class="block">
-            Trier par :
-          </label>
-          <select id="trie" v-model="selectedFilter" class="w-full px-3 py-1 rounded-md mb-2 shadow-md" placeholder="Trier par">
+          <label for="trie" class="block"> Trier par : </label>
+          <select
+            id="trie"
+            v-model="selectedFilter"
+            class="w-full px-3 py-1 rounded-md mb-2 shadow-md"
+            placeholder="Trier par"
+          >
             <option value="0">Aucun</option>
             <option value="1">Vulnerabilités</option>
             <option value="2">Outdated</option>
@@ -29,18 +73,23 @@
           </select>
         </div>
 
-        <div v-if="selectedFilter !== '0' && selectedFilter !== undefined" class="w-1/6">
-          <label for="sorting" class="block">
-            Ordre :
-          </label>
-          <select id="sorting" v-model="selectedOrder" class="w-full px-3 py-1 rounded-md mb-2 shadow-md" placeholder="ASC" @change="() => value = value.reverse()">
+        <div
+          v-if="selectedFilter !== '0' && selectedFilter !== undefined"
+          class="w-1/6"
+        >
+          <label for="sorting" class="block"> Ordre : </label>
+          <select
+            id="sorting"
+            v-model="selectedOrder"
+            class="w-full px-3 py-1 rounded-md mb-2 shadow-md"
+            placeholder="ASC"
+            @change="() => (value = value.reverse())"
+          >
             <option value="ASC">ASC</option>
             <option value="DESC">DESC</option>
           </select>
         </div>
       </div>
-      
-
     </div>
     <div class="w-full grid grid-cols-3 gap-3 lg:gap-9">
       <div
@@ -85,12 +134,12 @@ export default {
       value: undefined,
       imageToScan: undefined,
       search: undefined,
-      openModalAdd: false,
+      openImagesModal: false,
       isLoading: true,
-      selectedFilter: "0",
+      selectedFilter: '0',
       backupTags: undefined,
-      selectedOrder: "ASC",
-      openFilter: false
+      selectedOrder: 'ASC',
+      openFilter: false,
     }
   },
   computed: {
@@ -112,20 +161,34 @@ export default {
       if (newValue === '0') {
         this.value = this.tags
       } else if (newValue === '1') {
-        this.value = this.backupTags.sort((a,b) => (a.vulnerabilities > b.vulnerabilities) ? 1 : ((b.vulnerabilities > a.vulnerabilities) ? -1 : 0))
+        this.value = this.backupTags.sort((a, b) =>
+          a.vulnerabilities > b.vulnerabilities
+            ? 1
+            : b.vulnerabilities > a.vulnerabilities
+            ? -1
+            : 0
+        )
       } else if (newValue === '2') {
-        this.value = this.backupTags.sort((a,b) => (a.outdated_packages > b.outdated_packages) ? 1 : ((b.outdated_packages > a.outdated_packages) ? -1 : 0))
+        this.value = this.backupTags.sort((a, b) =>
+          a.outdated_packages > b.outdated_packages
+            ? 1
+            : b.outdated_packages > a.outdated_packages
+            ? -1
+            : 0
+        )
       } else if (newValue === '3') {
-        this.value = this.backupTags.sort((a,b) => (a.size > b.size) ? 1 : ((b.size > a.size) ? -1 : 0))
+        this.value = this.backupTags.sort((a, b) =>
+          a.size > b.size ? 1 : b.size > a.size ? -1 : 0
+        )
       } else if (newValue === '4') {
-        this.value = this.backupTags.sort((a,b) => {
-          if(a.vulnerabilities > 0) {
+        this.value = this.backupTags.sort((a, b) => {
+          if (a.vulnerabilities > 0) {
             return 1
           } else if (b.vulnerabilities > 0) {
             return -1
-          } 
+          }
 
-          if(a.outdated_packages > 0) {
+          if (a.outdated_packages > 0) {
             return 1
           } else if (b.outdated_packages > 0) {
             return -1
@@ -134,9 +197,15 @@ export default {
           }
         })
       } else if (newValue === '5') {
-        this.value = this.backupTags.sort((a,b) => (a.active_vulnerabilities > b.active_vulnerabilities) ? 1 : ((b.active_vulnerabilities > a.active_vulnerabilities) ? -1 : 0))
+        this.value = this.backupTags.sort((a, b) =>
+          a.active_vulnerabilities > b.active_vulnerabilities
+            ? 1
+            : b.active_vulnerabilities > a.active_vulnerabilities
+            ? -1
+            : 0
+        )
       }
-    }
+    },
   },
   async mounted() {
     await this.getTags()
@@ -149,6 +218,7 @@ export default {
       })
   },
   methods: {
+    ...mapActions('image', ['scanImages']),
     ...mapActions('image', ['getImages', 'getTags']),
     async sendImage() {
       await this.scanImages(this.imageToScan).then(() => {
@@ -164,6 +234,14 @@ export default {
       }
       return 'bg-green-500'
     },
+    async sendNewImage() {
+      this.isLoading = true
+      this.openImagesModal = false
+      await this.scanImages({image: this.imageName, return_error: false}).then(() => {
+        this.imageName = undefined
+        this.isLoading = false
+      })
+    }
   },
 }
 </script>
