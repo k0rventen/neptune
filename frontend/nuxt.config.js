a0dd715e872c9e89081a3973f7187002928e0b12
Kalv1 <calvin.lambert@protonmail.com>
2023-03-09 13:30:25 +0100

    :bug: Fix hardcoded backend URL

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -58,7 +58,7 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: 'https://neptune.map.interact-systemes-nancy.fr/'
+    baseURL: process.env.NUXT_APP_BACKEND_URL || 'http://localhost:3000'
   },
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
