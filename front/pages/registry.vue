d2f8d7a34a5f3a8c4ba5d3d331995b0272b1e657
Kalv1 <calvin.lambert@protonmail.com>
2024-06-22 13:13:49 +0200

    post on registry

--- front/pages/registry.vue
+++ front/pages/registry.vue
@@ -1,13 +1,60 @@
 <script setup>
+import { useMutation, useQuery } from "@tanstack/vue-query";
 const formValues = ref({
-  url: "",
-  username: "",
+  registry: "",
+  user: "",
+  password: "",
 });
+
+const { data: registries } = useQuery({
+  queryKey: ["registries"],
+  queryFn: async () => {
+    const res = await fetch("http://localhost:5000/api/registries");
+    return res.json();
+  },
+});
+
+const mutation = useMutation({
+  onMutate: async (variables) => {
+    await fetch("http://localhost:5000/api/registries", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(variables),
+    });
+  },
+  onError: (error, variables, context) => {
+    console.log(error);
+  },
+  onSuccess: (data, variables, context) => {
+    console.log(data);
+  },
+});
+
+const sendRegistry = () => {
+  mutation.mutate(formValues.value);
+};
 </script>
 
 <template>
   <div class="flex-1 p-5 grid grid-cols-1 lg:grid-rows-8 gap-2 relative z-[5]">
-    <card class="row-span-5"> </card>
+    <card class="row-span-5 grid grid-cols-4 grid-rows-3 gap-3">
+      <div
+        class="bg-[#242424] px-3 py-2 col-span-1 relative overflow-hidden rounded"
+        v-for="registry in registries"
+      >
+        <Icon
+          size="150"
+          name="iconoir:box-iso"
+          color="white"
+          class="opacity-50 absolute -right-8 -bottom-8"
+        />
+        <p>Registry: {{ registry.registry }}</p>
+        <p>Username: {{ registry.user }}</p>
+        <p>Password: {{ registry.password }}</p>
+      </div>
+    </card>
 
     <div class="row-span-3">
       <div class="flex gap-2">
@@ -21,12 +68,15 @@ const formValues = ref({
       </div>
 
       <card class="h-[calc(100%-32px)]">
-        <form class="flex flex-col gap-3 h-full justify-between">
+        <form
+          class="flex flex-col gap-3 h-full justify-between"
+          @submit.prevent="sendRegistry"
+        >
           <div class="gap-3 flex flex-col">
             <div class="flex gap-3">
               <label>Repository URL: </label>
               <input
-                v-model="formValues.url"
+                v-model="formValues.registry"
                 class="bg-transparent outline-none border-b-[1px] border-white/15"
                 type="text"
               />
@@ -34,7 +84,7 @@ const formValues = ref({
             <div class="flex gap-3">
               <label>Username: </label>
               <input
-                v-model="formValues.username"
+                v-model="formValues.user"
                 class="bg-transparent outline-none border-b-[1px] border-white/15"
                 type="text"
               />
